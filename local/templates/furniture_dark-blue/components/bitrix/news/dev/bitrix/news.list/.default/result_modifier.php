6b5822e5b2a6bcb2404130df923ab3d203c7e95d
Kate <Katrinekatek@gmail.com>
2019-10-22 13:49:02 +0300

    ex2-34

---
+++ local/templates/furniture_dark-blue/components/bitrix/news/dev/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+
+//
+//if($arParams["SPECIALDATE"]=="Y")
+//{
+//    foreach ($arResult["ITEMS"] as $item)
+//    {
+//        $APPLICATION->SetPageProperty("SPECIALDATE", $item["DISPLAY_ACTIVE_FROM"]);
+//        break;
+//    }
+//}
